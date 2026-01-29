41e59b1399f4c14c4a05825988ca0e5972c0b765
mr.kechen <mr-kechen@mr.ubuntu>
2026-01-30 07:52:25 +0800

    icon change2

--- island2localsend.cpp
+++ island2localsend.cpp
@@ -588,7 +588,7 @@ private:
             // 尝试使用灰色变体图标
             app_indicator_set_icon_full(
                 app_indicator,
-                "process-stop",  // 灰色发送图标
+                "view-restore",  // 灰色发送图标
                 "LocalSend 发送器 (已禁用)"
             );
         }
